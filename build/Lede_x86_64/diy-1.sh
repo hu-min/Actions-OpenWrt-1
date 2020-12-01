ebf612daed25aa65411e7713a3762c0d44974298
281677160 <66651997+281677160@users.noreply.github.com>
2020-12-01 09:59:43 +0800

    Update diy-1.sh

--- build/Lede_x86_64/diy-1.sh
+++ build/Lede_x86_64/diy-1.sh
@@ -12,3 +12,12 @@ rm -rf package/lean/autocore
 git clone https://github.com/garypang13/autocore package/lean/autocore
 
 sed -i 's/samba/samba4/' package/lean/default-settings/files/zzz-default-settings
+
+packages=" \
+brcmfmac-firmware-43430-sdio brcmfmac-firmware-43455-sdio kmod-brcmfmac wpad \
+kmod-fs-ext4 kmod-fs-vfat kmod-fs-exfat dosfstools e2fsprogs antfs-mount \
+kmod-usb2 kmod-usb3 kmod-usb-storage kmod-usb-storage-extras kmod-usb-storage-uas \
+kmod-usb-net kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8150 kmod-usb-net-rtl8152 \
+blkid lsblk parted fdisk cfdisk losetup resize2fs tune2fs pv unzip \
+lscpu htop iperf3 curl lm-sensors install-program 
+"
