6c15e4ffd70bce60cf9cd93cb58d7d51b1f6f7a6
281677160 <66651997+281677160@users.noreply.github.com>
2020-12-15 15:25:19 +0800

    Update AutoUpdate.sh

--- build/Lede_x86_64/Customize/AutoUpdate.sh
+++ build/Lede_x86_64/Customize/AutoUpdate.sh
@@ -1,125 +1,120 @@
 #!/bin/bash
 # https://github.com/Hyy2001X/AutoBuild-Actions
 # AutoBuild Module by Hyy2001
-# AutoUpdate for Openwrt
+# AutoBuild Actions
 
-Version=V4.3
-DEFAULT_DEVICE=QEMU Virtual CPU version 2.5+ : 1 Core 1 Thread
-Github=https://github.com/281677160/Actions-OpenWrt
-
-TIME() {
-	echo -ne "\n[$(date "+%H:%M:%S")] "
+Diy_Core() {
+Author=281677160
+Default_Device=x86-64-generic
 }
 
-Github_Download=${Github}/releases/download/AutoUpdate
-Author=${Github##*com/}
-Github_Tags=https://api.github.com/repos/${Author}/releases/latest
-cd /etc
-CURRENT_VERSION=$(awk 'NR==1' openwrt_info)
-CURRENT_DEVICE=$(jsonfilter -e '@.model.id' < "/etc/board.json" | tr ',' '_')
-clear && echo "Openwrt-AutoUpdate Script ${Version}"
-if [[ -z "$1" ]];then
-	Upgrade_Options="-q" && TIME && echo "执行: 保留配置更新固件"
-else
-	Upgrade_Options="$1"
-	[[ "${Upgrade_Options}" == "-n" ]] && TIME && echo "执行: 不保留配置更新固件"
-	if [[ "${Upgrade_Options}" == "-x" ]];then
-		Upgrade_Options="-q"
-		Force_Update="1"
-		TIME && echo "执行: 保留配置强制更新"
+Diy-Part1() {
+[ ! -d package/lean ] && mkdir -p package/lean
+
+Replace_File AutoUpdate.sh package/base-files/files/bin
+
+ExtraPackages git lean luci-app-autoupdate https://github.com/Hyy2001X main
+
+}
+
+Diy-Part2() {
+GET_TARGET_INFO
+echo "Author: $Author"
+echo "Openwrt Version: $Openwrt_Version"
+echo "AutoUpdate Version: $AutoUpdate_Version"
+echo "Router: $TARGET_PROFILE"
+echo "$Openwrt_Version" > package/base-files/files/etc/openwrt_info
+}
+
+Diy-Part3() {
+GET_TARGET_INFO
+Default_Firmware=openwrt-$TARGET_BOARD-$TARGET_SUBTARGET-$TARGET_PROFILE-squashfs-combined.img.gz
+AutoBuild_Firmware=AutoBuild-$TARGET_PROFILE-Lede-${Openwrt_Version}.img.gz
+AutoBuild_Detail=AutoBuild-$TARGET_PROFILE-Lede-${Openwrt_Version}.detail
+mkdir -p bin/Firmware
+echo "Firmware: $AutoBuild_Firmware"
+mv bin/targets/$TARGET_BOARD/$TARGET_SUBTARGET/$Default_Firmware bin/Firmware/$AutoBuild_Firmware
+echo "[$(date "+%H:%M:%S")] Calculating MD5 and SHA256 ..."
+Firmware_MD5=$(md5sum bin/Firmware/$AutoBuild_Firmware | cut -d ' ' -f1)
+Firmware_SHA256=$(sha256sum bin/Firmware/$AutoBuild_Firmware | cut -d ' ' -f1)
+echo -e "MD5: $Firmware_MD5\nSHA256: $Firmware_SHA256"
+touch bin/Firmware/$AutoBuild_Detail
+echo -e "\nMD5:$Firmware_MD5\nSHA256:$Firmware_SHA256" >> bin/Firmware/$AutoBuild_Detail
+}
+
+GET_TARGET_INFO() {
+Diy_Core
+[ -e $GITHUB_WORKSPACE/build/Lede_x86_64/Openwrt.info ] && . $GITHUB_WORKSPACE/build/Lede_x86_64/Openwrt.info
+AutoUpdate_Version=$(awk 'NR==6' package/base-files/files/bin/AutoUpdate.sh | awk -F '[="]+' '/Version/{print $2}')
+Default_File="package/lean/default-settings/files/zzz-default-settings"
+Lede_Version=$(egrep -o "R[0-9]+\.[0-9]+\.[0-9]+" $Default_File)
+Openwrt_Version="$Lede_Version-$Compile_Date"
+TARGET_PROFILE=$(egrep -o "CONFIG_TARGET.*DEVICE.*=y" .config | sed -r 's/.*DEVICE_(.*)=y/\1/')
+[ -z "$TARGET_PROFILE" ] && TARGET_PROFILE="$Default_Device"
+TARGET_BOARD=$(awk -F '[="]+' '/TARGET_BOARD/{print $2}' .config)
+TARGET_SUBTARGET=$(awk -F '[="]+' '/TARGET_SUBTARGET/{print $2}' .config)
+}
+
+ExtraPackages() {
+PKG_PROTO=$1
+PKG_DIR=$2
+PKG_NAME=$3
+REPO_URL=$4
+REPO_BRANCH=$5
+
+[ -d package/$PKG_DIR ] && mkdir -p package/$PKG_DIR
+[ -d package/$PKG_DIR/$PKG_NAME ] && rm -rf package/$PKG_DIR/$PKG_NAME
+[ -d $PKG_NAME ] && rm -rf $PKG_NAME
+Retry_Times=3
+while [ ! -e $PKG_NAME/Makefile ]
+do
+	echo "[$(date "+%H:%M:%S")] Checking out package [$PKG_NAME] ..."
+	case $PKG_PROTO in
+	git)
+		git clone -b $REPO_BRANCH $REPO_URL/$PKG_NAME $PKG_NAME > /dev/null 2>&1
+	;;
+	svn)
+		svn checkout $REPO_URL/$PKG_NAME $PKG_NAME > /dev/null 2>&1
+	esac
+	if [ -e $PKG_NAME/Makefile ] || [ -e $PKG_NAME/README* ];then
+		echo "[$(date "+%H:%M:%S")] Package [$PKG_NAME] is detected!"
+		mv $PKG_NAME package/$PKG_DIR
+		break
 	else
-		Force_Update="0"
+		[ $Retry_Times -lt 1 ] && echo "[$(date "+%H:%M:%S")] Skip check out package [$PKG_NAME] ..." && break
+		echo "[$(date "+%H:%M:%S")] [Error] [$Retry_Times] Checkout failed,retry in 3s ..."
+		Retry_Times=$(($Retry_Times - 1))
+		rm -rf $PKG_NAME > /dev/null 2>&1
+		sleep 3
 	fi
-fi
-opkg list | awk '{print $1}' > /tmp/Package_list
-grep "curl" /tmp/Package_list > /dev/null 2>&1
-if [[ ! $? -ne 0 ]];then
-	Google_Check=$(curl -I -s --connect-timeout 5 www.google.com -w %{http_code} | tail -n1)
-	[ ! "$Google_Check" == 200 ] && TIME && echo "Google 连接失败,可能导致固件下载速度缓慢!"
-fi
-grep "wget" /tmp/Package_list > /dev/null 2>&1
-if [[ $? -ne 0 ]];then
-	if [[ "${Force_Update}" == "1" ]];then
-		Choose="Y"
-	else
-		TIME && read -p "未安装[wget],是否执行安装?[Y/n]:" Choose
-	fi
-	if [[ "${Choose}" == Y ]] || [[ "${Choose}" == y ]];then
-		TIME && echo -e "开始安装[wget],请耐心等待...\n"
-		opkg update > /dev/null 2>&1
-		opkg install wget
-	else
-		TIME && echo "用户已取消安装,即将退出更新脚本..."
-		sleep 2
-		exit
-	fi
-fi
-if [[ -z "${CURRENT_VERSION}" ]];then
-	TIME && echo "警告: 当前固件版本获取失败!"
-	CURRENT_VERSION="未知"
-fi
-if [[ -z "${CURRENT_DEVICE}" ]];then
-	[[ "${Upgrade_Options}" == "-x" ]] && exit
-	TIME && echo "警告: 当前设备名称获取失败,使用预设名称[$DEFAULT_DEVICE]"
-	CURRENT_DEVICE="${DEFAULT_DEVICE}"
-fi
-TIME && echo "正在检查版本更新..."
-[ ! -f /tmp/Github_Tags ] && touch /tmp/Github_Tags
-wget -q ${Github_Tags} -O - > /tmp/Github_Tags
-GET_FullVersion=$(cat /tmp/Github_Tags | egrep -o "AutoBuild-${CURRENT_DEVICE}-Lede-R[0-9]+.[0-9]+.[0-9]+.[0-9]+" | awk 'END {print}')
-GET_Version="${GET_FullVersion#*Lede-}"
-if [[ -z "${GET_FullVersion}" ]] || [[ -z "${GET_Version}" ]];then
-	TIME && echo "检查更新失败,请稍后重试!"
-	exit
-fi
-echo -e "\n固件作者: ${Author%/*}"
-echo "设备名称: ${DEFAULT_DEVICE}"
-echo -e "\n当前固件版本: ${CURRENT_VERSION}"
-echo -e "云端固件版本: ${GET_Version}"
-if [[ ! ${Force_Update} == 1 ]];then
-	if [[ "${CURRENT_VERSION}" == "${GET_Version}" ]];then
-		TIME && read -p "已是最新版本,是否强制更新固件?[Y/n]:" Choose
-		if [[ "${Choose}" == Y ]] || [[ "${Choose}" == y ]];then
-			TIME && echo -e "开始强制更新固件...\n"
+done
+}
+
+Replace_File() {
+FILE_NAME=$1
+PATCH_DIR=$GITHUB_WORKSPACE/build/Lede_x86_64/openwrt/$2
+FILE_RENAME=$3
+
+[ ! -d $PATCH_DIR ] && mkdir -p $PATCH_DIR
+if [ -f $GITHUB_WORKSPACE/build/Lede_x86_64/Customize/$FILE_NAME ];then
+	if [ -e $GITHUB_WORKSPACE/build/Lede_x86_64/Customize/$FILE_NAME ];then
+		echo "[$(date "+%H:%M:%S")] Customize File [$FILE_NAME] is detected!"
+		if [ -z $FILE_RENAME ];then
+			[ -e $PATCH_DIR/$FILE_NAME ] && rm -f $PATCH_DIR/$FILE_NAME
+			mv -f $GITHUB_WORKSPACE/build/Lede_x86_64/Customize/$FILE_NAME $PATCH_DIR/$1
 		else
-			TIME && echo "已取消强制更新,即将退出更新程序..."
-			sleep 2
-			exit
+			[ -e $PATCH_DIR/$FILE_NAME ] && rm -f $PATCH_DIR/$3
+			mv -f $GITHUB_WORKSPACE/build/Lede_x86_64/Customize/$FILE_NAME $PATCH_DIR/$3
 		fi
+	else
+		echo "[$(date "+%H:%M:%S")] Customize File [$FILE_NAME] is not detected,skip move ..."
+	fi
+else
+	if [ -d $GITHUB_WORKSPACE/build/Lede_x86_64/Customize/$FILE_NAME ];then
+		echo "[$(date "+%H:%M:%S")] Customize Folder [$FILE_NAME] is detected !"
+		mv -f $GITHUB_WORKSPACE/build/Lede_x86_64/Customize/$FILE_NAME $PATCH_DIR
+	else
+		echo "[$(date "+%H:%M:%S")] Customize Folder [$FILE_NAME] is not detected,skip move ..."
 	fi
 fi
-Firmware_Info="${GET_FullVersion}"
-Firmware="${Firmware_Info}.img.gz"
-Firmware_Detail="${Firmware_Info}.detail"
-echo "云端固件名称: ${Firmware}"
-cd /tmp
-TIME && echo "正在下载固件,请耐心等待..."
-wget -q ${Github_Download}/${Firmware} -O ${Firmware}
-if [[ ! $? == 0 ]];then
-	TIME && echo "固件下载失败,请检查网络后重试!"
-	exit
-fi
-TIME && echo "下载成功!固件大小:$(du -h ${Firmware} | awk '{print $1}')B"
-TIME && echo "正在获取云端固件MD5,请耐心等待..."
-wget -q ${Github_Download}/${Firmware_Detail} -O ${Firmware_Detail}
-if [[ ! $? == 0 ]];then
-	TIME && echo "MD5 获取失败,请检查网络后重试!"
-	exit
-fi
-GET_MD5=$(awk -F '[ :]' '/MD5/ {print $2;exit}' ${Firmware_Detail})
-CURRENT_MD5=$(md5sum ${Firmware} | cut -d ' ' -f1)
-echo -e "\n本地固件MD5:${CURRENT_MD5}"
-echo "云端固件MD5:${GET_MD5}"
-if [[ -z "${GET_MD5}" ]] || [[ -z "${CURRENT_MD5}" ]];then
-	echo -e "\nMD5 获取失败!"
-	exit
-fi
-if [[ ! "${GET_MD5}" == "${CURRENT_MD5}" ]];then
-	echo -e "\nMD5 对比失败,请检查网络后重试!"
-	exit
-else
-	TIME && echo -e "MD5 对比通过!"
-fi
-TIME && echo -e "开始更新固件,请耐心等待路由器重启...\n"
-sleep 3
-sysupgrade ${Upgrade_Options} ${Firmware}
+}
